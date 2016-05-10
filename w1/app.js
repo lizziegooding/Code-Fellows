422a08c6bb3978be172be7e5e47de770a3a70acf
Lizzie Gooding <gooding.lizzie@gmail.com>
2016-05-10 12:47:22 -0700

    Finished writing quiz user function, about to start debugging

--- w1/app.js
+++ w1/app.js
@@ -8,28 +8,38 @@ if (userAction.addEventListener)
 else if (userAction.attachEvent)
     userAction.attachEvent('onclick', quizUser);
 
-var quizMe = function(){
+var quizUser = function(){
+  //Get user's name
   var userName = prompt('Thank you for taking this quiz. What is your name?');
   console.log('The user\'s name is ' + userName);
+  //Ask which city Lizzie studied abroad in
   var city = prompt('Ok ' + userName + ', let\'s begin. In what city did Lizzie study abroad?');
-  console.log('The user thinks Lizzie studied abroad in ' + city )
+  console.log('The user thinks Lizzie studied abroad in ' + city);
   if (city.toLowerCase() === 'valparaiso'){
     alert('Congratulations! Lizzie did stuy abroad in Valparaiso');
   } else {
     alert('Oops! Actually, Lizzie studied aborad in Valparaiso. Let\'s try another question.');
     }
-  }
+  //Ask if Lizzie knows C++
   var skills = prompt('Yes or No; Lizzie is skilled in C++ (Enter \'Y\' or \'N\')');
   if (skills.toLowerCase() === 'n'){
-    alert('Well done! Lizzie is not proficient in C++');
+    alert('Well done! Lizzie is not proficient in C++.');
     console.log('The user is right, Lizzie doesn\'t know CSS');
   } else {
-    alert('Oops! Actually, Lizzie studied aborad in Valparaiso. Let\'s try another question.');
+    alert('Oops! Actually, Lizzie is not proficient in C++.');
     console.log('The user is incorrect, Lizzie doesn\'t know CSS');
     }
+//Ask Lizzie's major in college
+    var major = prompt('Ok, final question. What did Lizzie study in college?');
+    if (major.toLowerCase() === 'geography'){
+      alert('Well done! Lizzie did study Geography in college.');
+      console.log('The user is right, Lizzie did study Geography');
+    } else {
+      alert('Oops! Actually, Lizzie did not study ' + major + '. She actually studied Geography.');
+      console.log('The user is incorrect, Lizzie didn\'t study '+ major);
+      }
   }
-  console.log('The user thinks Lizzie is ' + city ))
-}
+/*
 var userName = prompt('What is your name?');
 console.log('The user said there name is ' + userName);
 
@@ -53,4 +63,4 @@ while (keepAsking) {
     alert('Ooops!, you should have given a Y or N, so you loose this round.');
   }
 
-} // while
+} // while */
