f6370b30ce6140edddc1f0cc63ea3c53a77c7d6d
Lizzie Gooding <gooding.lizzie@gmail.com>
2016-05-10 12:38:25 -0700

    Added questions 1-3 to the quiz

--- w1/app.js
+++ w1/app.js
@@ -2,11 +2,34 @@
 // Demo code for lecture & lab
 
 var userAction = document.getElementById("quizMe");
+//From Stackoverflow question found here: http://stackoverflow.com/questions/1947263/using-an-html-button-to-call-a-javascript-function
 if (userAction.addEventListener)
     userAction.addEventListener("click", quizUser, false);
 else if (userAction.attachEvent)
     userAction.attachEvent('onclick', quizUser);
 
+var quizMe = function(){
+  var userName = prompt('Thank you for taking this quiz. What is your name?');
+  console.log('The user\'s name is ' + userName);
+  var city = prompt('Ok ' + userName + ', let\'s begin. In what city did Lizzie study abroad?');
+  console.log('The user thinks Lizzie studied abroad in ' + city )
+  if (city.toLowerCase() === 'valparaiso'){
+    alert('Congratulations! Lizzie did stuy abroad in Valparaiso');
+  } else {
+    alert('Oops! Actually, Lizzie studied aborad in Valparaiso. Let\'s try another question.');
+    }
+  }
+  var skills = prompt('Yes or No; Lizzie is skilled in C++ (Enter \'Y\' or \'N\')');
+  if (skills.toLowerCase() === 'n'){
+    alert('Well done! Lizzie is not proficient in C++');
+    console.log('The user is right, Lizzie doesn\'t know CSS');
+  } else {
+    alert('Oops! Actually, Lizzie studied aborad in Valparaiso. Let\'s try another question.');
+    console.log('The user is incorrect, Lizzie doesn\'t know CSS');
+    }
+  }
+  console.log('The user thinks Lizzie is ' + city ))
+}
 var userName = prompt('What is your name?');
 console.log('The user said there name is ' + userName);
 
